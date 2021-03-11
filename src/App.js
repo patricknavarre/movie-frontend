13edf8864eaf6e172de5b9494c7451413f8a0dae
Patrick Navarre <patrick.navarre@codeimmersives.com>
2021-03-11 17:47:33 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Movie from "./component/Movie"
+
+
+function App() {
+  return (
+    <div >
+
+    <Movie />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Movie.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class Movie extends Component {
+    render() {
+        return (
+            <div style={{marginTop: 20, textAlign: "center"}}>
+            <input />
+
+            <button>SUBMIT</button>  
+            </div>
+        )
+    }
+}
+
+export default Movie
