a4b3b943354305401cde1eb64cc5d752321ffc04
Patrick Navarre <patrick.navarre@codeimmersives.com>
2021-03-11 17:58:38 -0500

    onChange-added

--- src/component/Movie.js
+++ src/component/Movie.js
@@ -1,15 +1,42 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import axios from "axios";
 
 export class Movie extends Component {
-    render() {
-        return (
-            <div style={{marginTop: 20, textAlign: "center"}}>
-            <input />
 
-            <button>SUBMIT</button>  
-            </div>
-        )
+    state = {
+        movieInput: ""
     }
+
+    handleMovieInputOnChange = (event) => {
+
+        console.log(event.target.name);
+        this.setState({
+            [event.target.name]: event.target.value,
+        });
+
+        this.setState({
+            movieInput: event.target.value,
+        });
+    }
+
+  render() {
+    return (
+      <div style={{ marginTop: 20, textAlign: "center" }}>
+        <div style={{ marginTop: 20 }}>
+          <input 
+          type="text" 
+          name="movieInput" 
+          value={this.state.movieInput}
+          onChange={this.handleMovieInputOnChange}
+        />
+        </div>
+
+        <br />
+
+        <button>SUBMIT</button>
+      </div>
+    );
+  }
 }
 
-export default Movie
+export default Movie;
