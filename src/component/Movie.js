9d6647ba81a39fdcf216aa2d1beac77c5eb105b7
Patrick Navarre <patrick.navarre@codeimmersives.com>
2021-03-15 10:08:41 -0400

    updated

--- src/component/Movie.js
+++ src/component/Movie.js
@@ -4,20 +4,84 @@ import axios from "axios";
 export class Movie extends Component {
 
     state = {
-        movieInput: ""
+        movieList: [],
+        movieInput: "", 
+        isToggle: false,
+        updatedInput: "",
+    };
+
+    componentDidMount = async () => {
+        try {
+            let allMovie = await axios.get(
+                "http://localhost:3001/movie/get-all-movie"
+            );
+
+            console.log(allMovie);
+
+            this.setState({
+                movieList: allMovie.data.data,
+            });
+
+        } catch (e) {
+        console.log(e);
+        }
     }
 
     handleMovieInputOnChange = (event) => {
 
         console.log(event.target.name);
+ 
+
         this.setState({
             [event.target.name]: event.target.value,
         });
 
-        this.setState({
-            movieInput: event.target.value,
-        });
+        // this.setState({
+        //     movieInput: event.target.value,
+        // });
     }
+
+    handleMovieSubmit =  async () => {
+        try {
+
+            let createdMovie = await axios.post(
+                "http://localhost:3001/movie/create-movie",
+                { movie: this.state.movieInput }
+            );
+
+            console.log(createdMovie);
+
+            let newMovieList = [createdMovie.data.newMovie]
+
+            this.setState({
+                movieList: newMovieList,
+            });
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    handleDeleteById = async (id) => {
+        try {
+          let deletedMovie = await axios.delete(
+            "http://localhost:3001/movie/delete-movie",
+            {
+              data: { movieID: id },
+            }
+          );
+    
+        let newDeletedMovieArrayList = this.state.movieList.filter(
+            (item) => item._id !== deletedMovie.data.data._id
+        );
+          
+        this.setState({
+            movieList: newDeletedMovieArrayList,
+        });
+        
+        } catch (e) {
+            console.log(e);
+        }
+      };
 
   render() {
     return (
@@ -33,8 +97,14 @@ export class Movie extends Component {
 
         <br />
 
-        <button>SUBMIT</button>
-      </div>
+        <button style={{marginBottom: 10 }}onClick={this.handleMovieSubmit}>
+        SUBMIT
+        </button>
+
+        <br />
+
+        {this.state.movieList}
+    </div>
     );
   }
 }
